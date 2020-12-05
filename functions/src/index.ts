c99a73242564f1f6cd28deebb176e036cadda8e5
abmfahimshahriar <abmfahimshahriar@iut-dhaka.edu>
2020-12-05 06:35:31 +0600

    set up express

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,25 +1,28 @@
 import * as functions from "firebase-functions";
 // const admin = require('firebase-admin');
 import * as admin from "firebase-admin";
+import * as express from "express";
+const app = express();
+
 
 admin.initializeApp();
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
-  //   functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello world!");
-});
 
-export const getScreams = functions.https.onRequest((req, res) => {
-  admin
+
+app.get('/screams',(req,res) => {
+    admin
     .firestore()
     .collection('screams')
+    .orderBy('createdAt','desc')
     .get()
     .then((data) => {
       let screams: any[] = [];
       data.forEach((doc) => {
-        screams.push(doc.data());
+        screams.push({
+            screamId: doc.id,
+            body: doc.data().body,
+            userHandle: doc.data().userHandle,
+            createdAt: doc.data().createdAt
+        });
       });
 
       return res.json(screams);
@@ -27,15 +30,12 @@ export const getScreams = functions.https.onRequest((req, res) => {
     .catch((err) => console.error(err));
 });
 
-export const createScreams   = functions.https.onRequest((req, res) => {
 
-    // if(req.method !== 'POST') {
-    //     return res.status(400).json({error: 'Method not allowed.'});
-    // }
+app.post('/scream',(req,res) => {
     const newScream = {
         body: req.body.body,
         userHandle: req.body.userHandle,
-        createdAt: admin.firestore.Timestamp.fromDate(new Date())
+        createdAt: new Date().toISOString()
     };
 
     admin
@@ -48,7 +48,10 @@ export const createScreams   = functions.https.onRequest((req, res) => {
         .catch((err) => {
             res.status(500).json({error: 'something went wrong'});
             console.error(err);
-        })
-  });
+        });
+});
+
+
+  exports.api = functions.https.onRequest(app);
 
 
