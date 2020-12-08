15df2e0fc53bc653d4be43bf2cfdb3c5a682c404
abmfahimshahriar <abmfahimshahriar@iut-dhaka.edu>
2020-12-09 00:20:07 +0600

    add get user details endpoint

--- functions/handlers/users.ts
+++ functions/handlers/users.ts
@@ -114,6 +114,31 @@ export const addUserDetails = (req:any,res:any) => {
     })
 }
 
+
+// get user details
+export const getAuthenticatedUser = (req:any,res:any) => {
+  let userData:any = {};
+  db.doc(`/users/${req.user.handle}`).get()
+    .then(doc => {
+      if(doc.exists) {
+        userData.credentials = doc.data();
+        return db.collection('likes').where('userHandle','==',req.user.handle).get();
+      }
+      return;
+    })
+    .then(data => {
+      userData.likes = [];
+      data?.forEach(doc => {
+        userData.likes.push(doc.data());
+      });
+      return res.status(200).json(userData);
+    })
+    .catch(err => {
+      console.error(err);
+      return res.status(500).json({error: err.code});
+    });
+}
+
 // upload image
 export const uploadImage = (req: any, res: any) => {
   const busboy = new Busboy({ headers: req.headers });
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,7 @@
 import * as functions from "firebase-functions";
 import * as express from "express";
 import { getAllScreams, postOneScream } from "../handlers/screams";
-import { signup, login, uploadImage,addUserDetails } from "../handlers/users";
+import { signup, login, uploadImage,addUserDetails, getAuthenticatedUser } from "../handlers/users";
 import { FBAuth } from "../util/fbAuth";
 
 const app = express();
@@ -15,6 +15,7 @@ app.post("/signup", signup);
 app.post("/login", login);
 app.post("/user/image",FBAuth, uploadImage);
 app.post("/user",FBAuth,addUserDetails);
+app.get("/user",FBAuth, getAuthenticatedUser);
 
 
 exports.api = functions.https.onRequest(app);
