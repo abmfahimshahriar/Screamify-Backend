e5329a6e241911e85d8301434c525b2202092de1
abmfahimshahriar <abmfahimshahriar@iut-dhaka.edu>
2020-12-12 22:36:31 +0600

    create notification on like

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,6 @@
 import * as functions from "firebase-functions";
 import * as express from "express";
+import { db } from "../util/admin";
 import {
   getAllScreams,
   postOneScream,
@@ -7,7 +8,7 @@ import {
   commentOnScream,
   likeScream,
   unlikeScream,
-  deleteScream
+  deleteScream,
 } from "../handlers/screams";
 import {
   signup,
@@ -24,10 +25,10 @@ const app = express();
 app.get("/screams", getAllScreams);
 app.post("/scream", FBAuth, postOneScream);
 app.get("/scream/:screamId", getScream);
-app.delete("/scream/:screamId",FBAuth, deleteScream)
+app.delete("/scream/:screamId", FBAuth, deleteScream);
 app.post("/scream/:screamId/comment", FBAuth, commentOnScream);
-app.get("/scream/:screamId/like",FBAuth,likeScream);
-app.get("/scream/:screamId/unlike",FBAuth,unlikeScream);
+app.get("/scream/:screamId/like", FBAuth, likeScream);
+app.get("/scream/:screamId/unlike", FBAuth, unlikeScream);
 
 // users routes
 app.post("/signup", signup);
@@ -37,3 +38,32 @@ app.post("/user", FBAuth, addUserDetails);
 app.get("/user", FBAuth, getAuthenticatedUser);
 
 exports.api = functions.https.onRequest(app);
+
+exports.createNotificationOnLike = functions.firestore
+  .document("likes/{id}")
+  .onCreate((snapshot) => {
+    db.doc(`/screams/${snapshot.data().screamId}`)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data()?.userHandle,
+            sender: snapshot.data().userHandle,
+            type: "like",
+            read: false,
+            screamId: doc.id,
+          });
+        }
+        return;
+      })
+      .then(() => {
+        return;
+      })
+      .catch((err) => {
+        console.error(err);
+        return;
+      });
+  });
+
+
